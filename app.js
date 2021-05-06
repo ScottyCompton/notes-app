9aaa70a3f64d00c54942906d439b2bf48d5a6156
Scott Lonis <scott.lonis@outlook.com>
2021-05-06 07:53:24 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const msg = require('./notes');
+
+
+console.log(msg())
---
+++ utils.js
@@ -0,0 +1,10 @@
+console.log('utils.js');
+
+const name = ('Howdy');
+
+const add = (x, y) => {
+    return x + y;
+}
+
+
+module.exports = add;
